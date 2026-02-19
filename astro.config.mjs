b79d05f50f12b5b82179bd2f258cb44b6ab3e64d
FuzzyMistborn <fuzzy@fuzzymistborn.com>
2026-02-18 21:39:28 -0500

    Fix search, improve mobile view

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,5 @@ export default defineConfig({
       allowedHosts: ['magrathea.ktz.ts.net'],
       host: true,
     },
-    build: {
-      rollupOptions: {
-        external: ['/pagefind/pagefind.js']
-      }
-    }
   },
 });
