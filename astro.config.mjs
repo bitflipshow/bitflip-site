0533fb4bc68d58ed5eeaace0406129f6b484448e
FuzzyMistborn <fuzzy@fuzzymistborn.com>
2026-02-21 15:35:01 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bitflip.show",
+  integrations: [sitemap()],
   vite: {
     server: {
       allowedHosts: ['magrathea.ktz.ts.net'],
